a102c9ac1f02a8a40d8910036ac50328edbb9be5
Nico Hauser <me@tyratox.ch>
2017-03-19 13:17:13 +0100

    Fixed export

--- actions/auth.js
+++ actions/auth.js
@@ -1,6 +1,6 @@
-import {fetchApi}		from 'booki-frontend-core/utilities/rest.js';
+import {fetchApi} from 'booki-frontend-core/utilities/rest.js';
 
-const receiveAccessToken = (accessToken) => {
+export const receiveAccessToken = (accessToken) => {
 	return {
 		type: 'RECEIVE_ACCESS_TOKEN',
 		accessToken
@@ -47,29 +47,29 @@ const receiveAuthUser = (user, receivedAt) => {
 
 export const fetchUser = (accessToken) => {
 	return (dispatch) => {
-		
+
 		dispatch(
 			requestUser(accessToken)
 		);
-		
+
 		return fetchApi('user/me', 'GET', {}, accessToken)
 		.then((user) => {
-			
+
 			if(user.permissions.indexOf('admin') !== -1){
 				dispatch(
 					receiveUser(user, Date.now())
 				);
-				
+
 			}else{
 				throw new Error('The user isn\'t allowed to access the dashboard');
 			}
-			
+
 		}).catch((error) => {
-			
+
 			dispatch(
 				failUserRequest(error)
 			);
-			
+
 			dispatch(push('/'));
 		});
 	};
