6804a14d61e086e178e1ac810777c2537dcefbeb
Nico Hauser <me@tyratox.ch>
2017-04-20 12:15:38 +0200

    Added new person

---
+++ reducers/person/new.js
@@ -0,0 +1,31 @@
+import {combineReducers}
+       from 'redux';
+
+const newPerson = (state = {}, action) => {
+
+	switch(action.type){
+		case 'CLEAR_NEW_PERSON':
+			return {};
+		case 'UPDATE_NEW_PERSON':
+			return {...state, ...action.person};
+		case 'POST_PERSON':
+			return {
+				...state,
+				isFetching: true,
+				didInvalidate: false
+			};
+		case 'FAIL_PERSON_POST':
+
+			return {
+				...state,
+				isFetching: false,
+				didInvalidate: false
+			};
+
+		default:
+			return state;
+
+	};
+};
+
+export default newPerson;
